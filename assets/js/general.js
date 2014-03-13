a3405cb1ffcc6e79b12912c75e8c157366e20342
Justin Gruber <justin.gruber12@gmail.com>
2014-03-13 16:11:25 -0400

    Getting closest stop to user and storing that stop information.

--- assets/js/general.js
+++ assets/js/general.js
@@ -39,29 +39,50 @@ function getLocation() {
 }
 
 function findClosest(lat, lon){
-	//var reader = new FileReader();
-
-	console.log(lat);
-	console.log(lon);
-
-	// $.getJSON( "assets/json/stops.json", function() {
-	//   console.log("success");
-	// });
-
 
 	$.ajax({
         url: 'assets/json/stops.json',
         dataType: 'json',
         success: function(json) {
-        	var stop = json.stop_code;
-            console.log(stop);
+        	var closestStop = 50;		/* Distance placeholder */
+        	var diffLat = 0;			/* Distance of the latitude from user to stop */
+        	var diffLon = 0;			/* Distance of the longititude from user to stop */
+        	var R = 6371; 				/* Radius of the Earth in KM */
+        	var stopLat = 0;			/* Bus stop's latitude */
+        	var userLat = lat.toRad();	/* Bus stop's latitude */
+        	var a = 0;					/* Haversine formula to calc the shortest distance over the Earth's surface between two points */
+        	var c = 0; 					/* Further part of formula */
+        	var d = 0;					/* Distance between the two points */
+        	var currCloseStop = null;	/* Closest stop to user (JSON object) */
+
+            for (i = 0; i < json.length; i++){
+	            
+	            diffLat = (lat - json[i].stop_lat).toRad();
+	            diffLon = (lon - json[i].stop_lon).toRad();
+	            stopLat = json[i].stop_lat.toRad();
+	            userLat = lat.toRad();
+
+	            //Haversine formula to calc the shortest difference over the Earth's surface between two points
+	            a = Math.sin(diffLat/2) * Math.sin(diffLat/2) +
+	                    Math.sin(diffLon/2) * Math.sin(diffLon/2) * Math.cos(stopLat) * Math.cos(userLat); 
+	            c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	            d = R * c;
+
+	            if (d < closestStop){
+	            	closestStop = d;
+	            	currCloseStop = json[i];
+	            }
+            }
+            
+            //Outputting the stop code and name for closest stop
+            console.log(currCloseStop.stop_code);
+            console.log(currCloseStop.stop_name);
+
         },
         error: function () {
         	alert("failed!");
         }
     });
-
-	//console.log("after ajax");
 }
 
 /* kick shit off */
@@ -69,3 +90,8 @@ $(document).ready(function(){
 	getLocation();
 });
 
+
+
+Number.prototype.toRad = function() {
+    return this * Math.PI / 180;
+}
